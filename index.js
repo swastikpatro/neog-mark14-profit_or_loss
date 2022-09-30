e2c2bd94108e7a0728492b53fd2ad4402d77985f
swastikpatro <patroswastik.jsdev@gmail.com>
2022-09-30 13:52:57 +0530

    fix: removed alerts, fixed calculateProfitOrLoss()

--- index.js
+++ index.js
@@ -5,47 +5,38 @@ const quantityInput = document.querySelector('#quantity-input');
 const currentPriceInput = document.querySelector('#current-price-input');
 const btnContainer = document.querySelector('.btn-container');
 const output = document.querySelector('.output-section');
-const alertText = document.querySelector('.alert');
 
-function alertMsg(type, msg, ms) {
-  const tID = setInterval(() => {
-    alertText.innerText = msg;
-    alertText.classList.add(`alert-${type}`);
-    alertText.classList.add('show-alert');
-  }, 0);
-
-  setTimeout(() => {
-    clearInterval(tID);
-    alertText.classList.remove(`alert-${type}`);
-    alertText.classList.remove('show-alert');
-  }, ms);
-}
-
-function showOutput(text, amount, amountPercent) {
-  const profitOrLossCondition = text.startsWith('p');
+function showOutput(text, amount, amountPercent, profitOrLossCondition) {
   output.innerHTML = `
   Hey, the ${text} is
   <span style='color: ${profitOrLossCondition ? 'green' : 'red'}'>
-  ${amount}
+  ${amount} Rs
   </span> 
-  Rs and the percent is 
+  and the ${text} percent is 
   <span style='color: ${profitOrLossCondition ? 'green' : 'red'}'>
   ${amountPercent}%
   </span> ${profitOrLossCondition ? '🎉' : '😕'}
   `;
 }
 
+function displayErrorMsg(text) {
+  output.innerHTML = `<span style="color:red">${text}</span>`;
+}
+
 function calculateProfitORLoss(initial, quant, current) {
-  if (initial < current) {
-    const myProfit = ((current - initial) * quant).toFixed(2);
-    const myProfitPercent = ((myProfit / initial) * 100).toFixed(2);
-    showOutput('profit', myProfit, myProfitPercent);
-  } else if (initial > current) {
+  const cp = initial * quant;
+  const sp = current * quant;
+
+  if (cp < sp) {
+    const myProfit = Number.isInteger(sp - cp) ? sp - cp : (sp - cp).toFixed(2);
+    const myProfitPercent = ((myProfit / cp) * 100).toFixed(2);
+    showOutput('profit', myProfit, myProfitPercent, true);
+  } else if (cp > sp) {
     // Loss
-    const myLoss = ((initial - current) * quant).toFixed(2);
-    const myLossPercent = ((myLoss / initial) * 100).toFixed(2);
-    showOutput('loss', myLoss, myLossPercent);
-  } else if (initial === current) {
+    const myLoss = Number.isInteger(cp - sp) ? cp - sp : (cp - sp).toFixed(2);
+    const myLossPercent = ((myLoss / cp) * 100).toFixed(2);
+    showOutput('loss', myLoss, myLossPercent, false);
+  } else if (cp === sp) {
     output.innerText = 'No Profit but No Loss. Invest in FD 😜';
   }
 }
@@ -66,28 +57,26 @@ function handleContainerClick(e) {
     quantityInput.value = '';
     currentPriceInput.value = '';
     output.innerText = '';
-    alertMsg('success', 'Cleared', 1000);
     return;
   }
 
   if (
     !(initialPriceInput.value && quantityInput.value && currentPriceInput.value)
   ) {
-    alertMsg('danger', 'Please fill all input fields 🙏', 1000);
+    displayErrorMsg('Please fill all input fields 🙏');
     return;
   }
   if (initialPrice <= 0 || currentPrice <= 0 || quantity <= 0) {
-    alertMsg('danger', "Prices or Quantity can't be negative ❌", 1000);
+    displayErrorMsg("Prices or Quantity can't be zero or negative ❌");
     return;
   }
 
   calculateProfitORLoss(initialPrice, quantity, currentPrice);
-  alertMsg('success', 'Done ✅', 1000);
 }
 
 btnContainer.addEventListener('click', handleContainerClick);
 
-document.querySelectorAll('input').forEach((singleInput) => {
+[...document.querySelectorAll('input')].forEach((singleInput) => {
   singleInput.addEventListener('click', () => {
     output.innerText = '';
   });
